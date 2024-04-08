aae0077a95f4530a2e136aa0ea8c519ec538427b
Jesus Lopez Reynosa <jesuslopezreynosa@fastmail.com>
2024-04-07 21:11:34 -0700

    Added DuckDNS

---
+++ DuckDNS/updateDomainDynamic.sh
@@ -0,0 +1,22 @@
+# Allows updating IPv4 and IPv6 (if applicable) addresses for a DuckDNS domain
+# name. Like the script provided from DuckDNS, the log file will say OK if 
+# good and KO if an error occurred
+domain="INSERT DOMAIN NAME HERE"
+duckDnsApiToken="xxxxxxx-xxxx-0000-0000-000000000"
+
+url="https://www.duckdns.org/update?domains=${domain}&token=${duckDnsApiToken}&ip="
+ipv6Addr=$(curl -6 ifconfig.co)
+ipv4Addr=$(curl -4 ifconfig.co)
+
+# Build Base URL
+url="$url$ipv4Addr"
+
+if [[ $ipv6Addr != *"Failed"* ]]
+then
+        ipv6Addr="&ipv6=$ipv6Addr"
+        url="$url$ipv6Addr"
+fi
+
+echo $url
+
+curl $url -o ~/duckdns/duck.log
